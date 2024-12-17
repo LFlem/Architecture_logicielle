d4b732cb30f675bfdb3c4d8bb5a11d83160b61de
linerol <sessi.tchecounnou@epitech.eu>
2024-12-17 23:18:37 +0100

    feat: add lower and upper decorator

--- src/decorators/isLowerThan.ts
+++ src/decorators/isLowerThan.ts
@@ -1,8 +1,8 @@
 import { MetadataManager } from '../core/MetadataManager';
 import { IsLowerThan } from '../validators/IsLowerThan';
 
-// export function isLowerThan(otherProperty: string) {
-//   return function (target: any, propertyKey: string) {
-//     MetadataManager.storeRule(target, propertyKey, new IsLowerThanValidator(otherProperty));
-//   };
-// }
+export function isLowerThan(reference: number | Date) {
+  return function (target: any, propertyKey: string) {
+    MetadataManager.storeRule(target, propertyKey, new IsLowerThan(reference));
+  };
+}
--- src/decorators/isUpperThan.ts
+++ src/decorators/isUpperThan.ts
@@ -1,8 +1,8 @@
 import { MetadataManager } from '../core/MetadataManager';
 import { IsUpperThan } from '../validators/IsUpperThan';
 
-// export function isUpperThan(otherProperty: string) {
-//   return function (target: any, propertyKey: string) {
-//     MetadataManager.storeRule(target, propertyKey, new IsUpperThan(otherProperty));
-//   };
-// }
+export function isUpperThan(reference: number | Date) {
+  return function (target: any, propertyKey: string) {
+    MetadataManager.storeRule(target, propertyKey, new IsUpperThan(reference));
+  };
+}
